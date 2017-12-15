82cfae9206f712027f8b73992d75961f317ecfab
LUCAS TAGLIANI AGUIAR <ALU201315059@uniritter.local>
2017-12-15 20:33:34 -0200

    adding capabilities to configuration

--- conf.js
+++ conf.js
@@ -2,5 +2,11 @@
 exports.config = {
 	// framework: 'jasmine',
 	seleniumAdress: 'http://localhost:4444/wd/hub',
-	specs: ['specs/login-spec.js']
+	specs: ['specs/login-spec.js'],
+	capabilities: {
+		browserName: 'chrome',
+		chromeOptions: {
+			args: [ "--headless", "--disable-gpu", "--windows-size=800,600"]
+		}
+	}
 }
