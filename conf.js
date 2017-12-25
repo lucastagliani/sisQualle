e6eeb1017db3b7b75a63141dd21a8143ee0ba214
andersonbaum <anderson.baum@gmail.com>
2017-12-25 16:17:22 -0200

    Ajustando variavel PASSWORD de senha

--- conf.js
+++ conf.js
@@ -1,12 +1,12 @@
 // conf.js
 exports.config = {
-	// framework: 'jasmine2',
+	// framework: 'jasmine',
 	seleniumAdress: 'http://localhost:4444/wd/hub',
 	specs: ['specs/login-spec.js'],
 	capabilities: {
 		browserName: 'chrome',
 		chromeOptions: {
 			args: [ "--headless", "--disable-gpu", "--windows-size=800,600", "--lang=pt"]
-		} 
+		}
 	}
 }
