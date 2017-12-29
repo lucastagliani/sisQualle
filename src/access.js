0f8a9186880d168f84b4488501dee43f56bd84ed
lucastagliani <lucas.tagliani@outlook.com>
2017-12-28 23:50:28 -0200

    creating file inside src

---
+++ src/access.js
@@ -0,0 +1,7 @@
+var baseUrl = 'https://app.zenvia.com';
+var email = 'lucas.tagliani@zenvia.com';
+var pass = process.env.PASSWORD;
+
+module.exports.baseUrl = baseUrl;
+module.exports.email = email;
+module.exports.pass = pass;
