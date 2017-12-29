a9d9809994a7eb398390a57c707a58593ccbae79
lucastagliani <lucas.tagliani@outlook.com>
2017-12-28 23:54:51 -0200

    removing duplicated file

--- access.js
+++
@@ -1,7 +0,0 @@
-var baseUrl = 'https://app.zenvia.com';
-var email = 'lucas.tagliani@zenvia.com';
-var pass = process.env.PASSWORD;
-
-module.exports.baseUrl = baseUrl;
-module.exports.email = email;
-module.exports.pass = pass;
